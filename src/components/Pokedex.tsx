60270c79059f133a84e14c3009f48b0da581d23a
Eduardo Cavalcante <42215395+EPEduardoPaixao@users.noreply.github.com>
2021-03-10 01:31:31 -0300

    ajuste para realizar o get ok

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -3,59 +3,63 @@ import pokeapi from "../services/pokeapi";
 
 // import { Container } from './styles';
 
-// interface IPokemon [{
-
-//     name:string;
-//     url:string;
-// }]
+// interface IPokemon {
+//   id: number;
+//   name: string;
+//   photo: string;
+// }
 
 const Pokedex: React.FC = () => {
-  const [pokemon, setPokemon] = useState(
-    [],
-  );
+  // const [pokemon, setPokemon] = useState<IPokemon[]>([]);
+  const [pokemon, setPokemon] = useState([{
+    name:'',
+    id:0,
+    photo:'',
+  }]);
 
   useEffect(() => {
-      let i =0
-    const getPokemon = async (id:number) => {
+    let i = 0;
+    const getPokemon = async (id: number) => {
       try {
         const { data } = await pokeapi.get(`pokemon/${id}`);
 
-        setPokemon(data.name);
-        console.log("pokemons", pokemon);
+        setPokemon([{ ...pokemon, name: data.name, id: data.id, photo: data.sprites.front_default}]);
+        // setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     };
-    for (i = 1; i < 150; i++) {
-        // const element = array[index];
-        
-        getPokemon(i);
-        console.log('i',i)
+    for (i = 1; i < 152; i++) {
+      // const element = array[index];
+
+      getPokemon(i);
+      // console.log("i", i);
     }
     // getPokemon()
   }, []);
+  console.log(pokemon);
 
   return (
     <div>
       <h1>Pokedex</h1>
-      <div style={{display:'flex', justifyContent: 'center'}}>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            padding: 10,
-            maxWidth: "50%",
-            justifyContent: "center",
-          }}
-        >
-          {pokemon!==undefined?(
-
-              pokemon.map((pokemons:any) => (
-                  <div style={{ padding: 10 }}>{pokemons.name}</div>
-                  ))
-                  ):(null)
-                }
-        </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              padding: 10,
+              maxWidth: "50%",
+              justifyContent: "center",
+            }}
+          >
+            {pokemon.map((pokemons) => (
+              <div>
+                <strong>{pokemons.name}</strong>
+                <img src={pokemons.photo} alt={pokemons.name} />
+                {/* {console.log(pokemon)} */}
+              </div>
+            ))}
+          </div>
       </div>
     </div>
   );
