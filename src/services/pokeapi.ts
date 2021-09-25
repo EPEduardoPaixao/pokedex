e21d7d2197a5ae467ab529e77dd951bed291201c
Eduardo Cavalcante <42215395+Eduardo-Paixao@users.noreply.github.com>
2021-09-25 14:09:18 -0300

    ajustes na pokeapi

--- src/services/pokeapi.ts
+++ src/services/pokeapi.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const pokeapi = axios.create({
+ const pokeapi = axios.create({
     baseURL:'https://pokeapi.co/api/v2/'
 })
 
-export default pokeapi;
+export default pokeapi
+
